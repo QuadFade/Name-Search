26ca6f746f4671f9c4cee9d59066bb74979714ce
MSI <MSI@MSI-PC>
2022-03-29 15:06:13 -0400

    Add project files.

---
+++ Name Search/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Name_Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.girlLabel = new System.Windows.Forms.Label();
+            this.boyLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.nameSearchButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.girlTextBox = new System.Windows.Forms.TextBox();
+            this.boyTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // girlLabel
+            // 
+            this.girlLabel.AutoSize = true;
+            this.girlLabel.Location = new System.Drawing.Point(9, 45);
+            this.girlLabel.Name = "girlLabel";
+            this.girlLabel.Size = new System.Drawing.Size(115, 13);
+            this.girlLabel.TabIndex = 0;
+            this.girlLabel.Text = "Search for a Girl name:";
+            // 
+            // boyLabel
+            // 
+            this.boyLabel.AutoSize = true;
+            this.boyLabel.Location = new System.Drawing.Point(9, 80);
+            this.boyLabel.Name = "boyLabel";
+            this.boyLabel.Size = new System.Drawing.Size(118, 13);
+            this.boyLabel.TabIndex = 1;
+            this.boyLabel.Text = "Search for a Boy name:";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(40, 9);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(213, 13);
+            this.descriptionLabel.TabIndex = 2;
+            this.descriptionLabel.Text = "Top 200 Boys and Girls Names (2000-2009)";
+            // 
+            // nameSearchButton
+            // 
+            this.nameSearchButton.Location = new System.Drawing.Point(64, 115);
+            this.nameSearchButton.Name = "nameSearchButton";
+            this.nameSearchButton.Size = new System.Drawing.Size(75, 39);
+            this.nameSearchButton.TabIndex = 3;
+            this.nameSearchButton.Text = "Search";
+            this.nameSearchButton.UseVisualStyleBackColor = true;
+            this.nameSearchButton.Click += new System.EventHandler(this.nameSearchButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(145, 115);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 39);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // girlTextBox
+            // 
+            this.girlTextBox.Location = new System.Drawing.Point(130, 42);
+            this.girlTextBox.Name = "girlTextBox";
+            this.girlTextBox.Size = new System.Drawing.Size(141, 20);
+            this.girlTextBox.TabIndex = 5;
+            // 
+            // boyTextBox
+            // 
+            this.boyTextBox.Location = new System.Drawing.Point(130, 77);
+            this.boyTextBox.Name = "boyTextBox";
+            this.boyTextBox.Size = new System.Drawing.Size(141, 20);
+            this.boyTextBox.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 168);
+            this.Controls.Add(this.boyTextBox);
+            this.Controls.Add(this.girlTextBox);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.nameSearchButton);
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.boyLabel);
+            this.Controls.Add(this.girlLabel);
+            this.Name = "Form1";
+            this.Text = "Name Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label girlLabel;
+        private System.Windows.Forms.Label boyLabel;
+        private System.Windows.Forms.Label descriptionLabel;
+        private System.Windows.Forms.Button nameSearchButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox girlTextBox;
+        private System.Windows.Forms.TextBox boyTextBox;
+    }
+}
+
---
+++ Name Search/Form1.cs
@@ -0,0 +1,138 @@
+﻿//John Fade IV 2307363
+//I worked alone on this assignment
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Name_Search
+{
+    public partial class Form1 : Form
+    {
+       
+        List<string> boysList = new List<string>();
+        List<string> girlsList = new List<string>();
+        public Form1()
+        {
+            //initilizes methods on form launch
+            InitializeComponent();
+            //triggers the read file methods at form launch
+            ReadGirls(girlsList);
+            ReadBoys(boysList);
+
+
+        }
+
+        //reads boys file
+        private void ReadBoys(List<string> boysList)
+        {
+            try
+            {
+                StreamReader inputFile = File.OpenText("BoyNames.txt"); //opens the text
+
+                while (!inputFile.EndOfStream)
+                {
+                    boysList.Add(inputFile.ReadLine()); //reads entire text and adds to boysList
+                }
+
+                inputFile.Close(); //closes the file
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        //reads girls file
+        private void ReadGirls(List<string> girlsList)
+        {
+            try
+            {
+                StreamReader inputFile = File.OpenText("GirlNames.txt"); //opens the text
+                while (!inputFile.EndOfStream)
+                {
+                    girlsList.Add(inputFile.ReadLine()); //reads entire text and adds to girlsList
+                }
+
+                inputFile.Close();
+                
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+
+        //search button click event
+            //determines if the name entered by the user matches one found in their respective listboxes
+            //outputs message boxes accordingly
+        private void nameSearchButton_Click(object sender, EventArgs e)
+        {
+            
+
+            string boysSearchText = boyTextBox.Text.Trim(); //holds the boy entry by the user
+            string girlsSearchText = girlTextBox.Text.Trim(); //holds the girl entry by the user
+            int girlCount; //holds the compared rank          
+            int boyCount; //holds the compared rank
+            bool boyNameFound = false; //boy default boolean set to false
+            bool girlNameFound = false; //girl default boolean set to false
+
+            //boys test to see if the entered text matches a name found in the list
+            
+            if (boysList.Contains(boysSearchText))
+            {
+                for  (boyCount = -1; boyCount <= boysList.IndexOf(boysSearchText); boyCount++)
+                {
+
+                }
+                MessageBox.Show(boysSearchText + " was found in the top 200! " + " Located at rank: #" + boyCount + " of 200!");
+                boyNameFound = true;
+            }
+            else
+            {
+                boyNameFound = false;
+            }
+
+            //girls test to see if the entered text matches a name found in the list
+            if (girlsList.Contains(girlsSearchText))
+            {
+                
+                for ( girlCount = -1; girlCount <= girlsList.IndexOf(girlsSearchText); girlCount++)
+                {
+                    
+                }               
+                MessageBox.Show(girlsSearchText + " was found in the top 200! " + "Located at rank: #" + girlCount + " of 200!");
+                girlNameFound = true;
+
+            }
+            else
+            {
+                girlNameFound = false;
+            }
+
+            //the boy and girl catches for failed entries
+            if (girlNameFound == false )
+            {
+                MessageBox.Show("A girls name you entered was not found.");
+            }
+            if (boyNameFound == false)
+            {
+                MessageBox.Show("A boys name you entered was not found.");
+            }
+        }
+        //exits the form
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
